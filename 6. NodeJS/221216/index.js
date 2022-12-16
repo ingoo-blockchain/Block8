989f7ead59b7e36122d00059ef3220a11b9dd659
ingoo <web7722@gmail.com>
2022-12-16 12:52:19 +0900

    fix: lotto.js

--- 6. NodeJS/221216/index.js
+++ 6. NodeJS/221216/index.js
@@ -1,52 +1,52 @@
-const btn = document.querySelector("#submit")
-const LottoDisplay = document.querySelector("#lottonum")
-const LottoBox = document.querySelectorAll("#backarea>#lottonum > li")
-const Payinfo = document.querySelector("#payinfo")
-const Info = document.querySelector("#info")
-const getLotto = () => {
-    const ClassPattern = {
-        0: "a",
-        1: "b",
-        2: "c",
-        3: "d",
-        4: "e",
-    }
+// const btn = document.querySelector("#submit")
+// const LottoDisplay = document.querySelector("#lottonum")
+// const LottoBox = document.querySelectorAll("#backarea>#lottonum > li")
+// const Payinfo = document.querySelector("#payinfo")
+// const Info = document.querySelector("#info")
+// const getLotto = () => {
+//     const ClassPattern = {
+//         0: "a",
+//         1: "b",
+//         2: "c",
+//         3: "d",
+//         4: "e",
+//     }
 
-    const getRandom = (start, end) => Math.floor(Math.random() * (end - start) + start)
-    const getNumber = (start, end) => (end < start ? null : getRandom(start, end))
+//     const getRandom = (start, end) => Math.floor(Math.random() * (end - start) + start)
+//     const getNumber = (start, end) => (end < start ? null : getRandom(start, end))
 
-    const randomLottoFn = (length, count) => {
-        const result = new Array(count).fill(null)
-        const lotto = new Array(length).fill(null).map((value, index) => index + 1)
-        return result
-            .map((v) => {
-                const lottoIndex = getNumber(1, lotto.length)
-                const [number] = lotto.splice(lottoIndex, 1)
-                return number
-            })
-            .sort((a, b) => a - b)
-    }
+//     const randomLottoFn = (length, count) => {
+//         const result = new Array(count).fill(null)
+//         const lotto = new Array(length).fill(null).map((value, index) => index + 1)
+//         return result
+//             .map((v) => {
+//                 const lottoIndex = getNumber(1, lotto.length)
+//                 const [number] = lotto.splice(lottoIndex, 1)
+//                 return number
+//             })
+//             .sort((a, b) => a - b)
+//     }
 
-    const between = (number) => Math.ceil(number / 10) - 1
-    const getClassName = (arr) => arr.map((v) => ClassPattern[between(v)])
+//     const between = (number) => Math.ceil(number / 10) - 1
+//     const getClassName = (arr) => arr.map((v) => ClassPattern[between(v)])
 
-    return {
-        randomLotto: randomLottoFn(45, 6),
-        numClassName: getClassName(this.randomLotto),
-    }
-}
-function lottoHandler(e) {
-    // const randomLotto = randomLottoFn(45, 6)
-    // const numClassName = getClassName(randomLotto)
-    const { randomLotto, numClassName } = getLotto()
+//     return {
+//         randomLotto: randomLottoFn(45, 6),
+//         numClassName: getClassName(this.randomLotto),
+//     }
+// }
+// function lottoHandler(e) {
+//     // const randomLotto = randomLottoFn(45, 6)
+//     // const numClassName = getClassName(randomLotto)
+//     const { randomLotto, numClassName } = getLotto()
 
-    for (let i = 0; i < LottoBox.length; i++) {
-        LottoBox[i].innerHTML = randomLotto[i]
-        LottoBox[i].className = numClassName[i]
-    }
+//     for (let i = 0; i < LottoBox.length; i++) {
+//         LottoBox[i].innerHTML = randomLotto[i]
+//         LottoBox[i].className = numClassName[i]
+//     }
 
-    LottoDisplay.style = "Display:flex;"
-    Info.style = "display:block;"
-    Payinfo.style = "Display:block;"
-}
-btn.addEventListener("click", lottoHandler)
+//     LottoDisplay.style = "Display:flex;"
+//     Info.style = "display:block;"
+//     Payinfo.style = "Display:block;"
+// }
+// btn.addEventListener("click", lottoHandler)
--- 6. NodeJS/221216/lotto.js
+++ 6. NodeJS/221216/lotto.js
@@ -1,31 +1,55 @@
-// const ClassPattern = {
-//     0: "a",
-//     1: "b",
-//     2: "c",
-//     3: "d",
-//     4: "e",
-// }
+const getLotto = ({ totalNumber, count }) => {
+    const result = new Array(count).fill(null)
+    const lotto = new Array(totalNumber).fill(null).map((value, index) => index + 1)
+    const ClassPattern = {
+        0: "a",
+        1: "b",
+        2: "c",
+        3: "d",
+        4: "e",
+    }
 
-// const getNumber = (start, end) => {
-//     const random = Math.floor(Math.random() * (end - start) + start)
-//     return end < start ? null : random
-// }
+    const getNumber = (start, end) => {
+        const random = Math.floor(Math.random() * (end - start) + start)
+        return end < start ? null : random
+    }
 
-// const randomLotto = (length, count) => {
-//     const result = new Array(count).fill(null)
-//     const lotto = new Array(length).fill(null).map((value, index) => index + 1)
-//     return result
-//         .map((v) => {
-//             const lottoIndex = getNumber(1, lotto.length)
-//             const [number] = lotto.splice(lottoIndex, 1)
-//             return number
-//         })
-//         .sort((a, b) => a - b)
-// }
+    const getSpliceNumber = (arr) => (v) => {
+        const lottoIndex = getNumber(1, arr.length)
+        const [number] = arr.splice(lottoIndex, 1)
+        return number
+    }
 
-// const between = (number) => Math.ceil(number / 10) - 1
-// const getClassName = (arr) => arr.map((v) => ClassPattern[between(v)])
-// const lottoArray = randomLotto(45, 6)
+    const randomLotto = () => result.map(getSpliceNumber(lotto)).sort((a, b) => a - b)
+    const between = (number) => Math.ceil(number / 10) - 1
+    const getClassName = (arr) => arr.map((v) => ClassPattern[between(v)])
 
-// // console.log(lottoArray)
-// // console.log(getClassName(lottoArray))
+    const lottoArr = randomLotto()
+
+    return {
+        lotto: lottoArr,
+        className: getClassName(lottoArr),
+    }
+}
+
+class Lotto {
+    constructor({ totalNumber, count }) {
+        this.lotto
+        this.className
+    }
+
+    getNumber() {}
+    getSpliceNumber() {}
+    randomLotto() {}
+    between() {}
+    getClassName() {}
+}
+
+const config = {
+    totalNumber: 45,
+    count: 6,
+}
+const { className, lotto: lottoArr } = getLotto(config)
+console.log(className, lottoArr)
+
+const a = new Lotto(config)
