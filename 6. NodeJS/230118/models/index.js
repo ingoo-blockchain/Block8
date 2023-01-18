789225b439dce0206c2dbe1a0e00bc07954fa73f
ingoo <web7722@gmail.com>
2023-01-18 09:46:14 +0900

    feat: sequelize setting

--- 6. NodeJS/230118/models/index.js
+++ 6. NodeJS/230118/models/index.js
@@ -1,3 +1,6 @@
+const fs = require("fs")
+const path = require("path")
+
 const Sequelize = require("sequelize")
 const env = process.env.NODE_NEV || "development"
 const config = require("../config")["db"][env]
@@ -5,8 +8,18 @@ const config = require("../config")["db"][env]
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
 // sequelize -> models
+fs.readdirSync(__dirname)
+    .filter((file) => file.indexOf("model") !== -1)
+    .forEach((file) => {
+        require(path.join(__dirname, file))(sequelize, Sequelize)
+    })
 
 // models -> 관계형
+const { models } = sequelize
+for (const key in models) {
+    if (typeof models[key].associate !== "function") continue
+    models[key].associate(models)
+}
 
 module.exports = {
     Sequelize,
--- 6. NodeJS/230118/server.js
+++ 6. NodeJS/230118/server.js
@@ -1,6 +1,9 @@
 const app = require("./app")
-// sequelize
+const { sequelize } = require("./models")
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log(`Running on http://localhost:3000`)
+app.listen(port, async () => {
+    await sequelize.sync({ force: false })
+    console.log(`Database Connected...`)
+    console.log(`Running on http://localhost:${port}`)
 })
