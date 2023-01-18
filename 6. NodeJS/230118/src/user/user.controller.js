f268dc701b4f661c846a16af5dc53d1819601052
ingoo <web7722@gmail.com>
2023-01-18 12:34:26 +0900

    feat:userController

---
+++ 6. NodeJS/230118/src/user/user.controller.js
@@ -0,0 +1,15 @@
+class UserController {
+    constructor({ userService }) {
+        this.userService = userService
+    }
+
+    async postSignup(req, res, next) {
+        try {
+            const { userid, userpw, username } = req.body
+            const user = await this.userService.signup({ userid, userpw, username })
+            res.status(201).json(user)
+        } catch (e) {
+            next(e)
+        }
+    }
+}
