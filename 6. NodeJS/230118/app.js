3600436a85d5cc3d4273957979ad96c559700425
ingoo <web7722@gmail.com>
2023-01-18 12:46:16 +0900

    feat: app.js

--- 6. NodeJS/230118/app.js
+++ 6. NodeJS/230118/app.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
+const router = require("./routes")
 
 app.use(express.json())
+app.use(router)
 // router..
 app.use((error, req, res, next) => {
     res.status(500).send(error.message)
